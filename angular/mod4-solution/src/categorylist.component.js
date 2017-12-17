b69c0456c0431f94aa4ba6cdc80b713af7786db5
jota <jotica@gmail.com>
2017-12-17 01:52:16 -0600

    remove comments

--- angular/mod4-solution/src/categorylist.component.js
+++ angular/mod4-solution/src/categorylist.component.js
@@ -1,19 +1,9 @@
 (function () {
   'use strict'; 
 
-  //function CategoryListController() { 
-  //  var ctrl = this; 
-
-  //  ctrl.categories = function () {
-  //    return ctrl.list;
-  //  }
-
-  //}
-
   angular.module('MenuApp')
     .component('categoryList', {
       templateUrl: 'src/templates/category-list.template.html',
-      //controller: CategoryListController,
       bindings: {
         list: '<'
       }
