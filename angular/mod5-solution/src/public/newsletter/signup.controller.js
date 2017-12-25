29bc10c59c56a423482abd673bcbe8abef98c31d
jota <jotica@gmail.com>
2017-12-25 05:19:38 -0600

    Ng 5 - Add signup function to signupCtrl

--- angular/mod5-solution/src/public/newsletter/signup.controller.js
+++ angular/mod5-solution/src/public/newsletter/signup.controller.js
@@ -4,8 +4,16 @@
 angular.module('public')
 .controller('NewsletterSignupController', NewsletterSignupController);
 
-function NewsletterSignupController() {
+NewsletterSignupController.$inject = [ 'RegistrationService'];
+function NewsletterSignupController(RegistrationService) {
   var signupCtrl = this;
+
+  signupCtrl.signup = function () {
+    RegistrationService.registerUser( signupCtrl.firstName,
+        signupCtrl.lastName,
+        signupCtrl.email,
+        signupCtrl.phone);
+  }
 }
 
 
